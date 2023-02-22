14640ff21b1e5f4b628078d7f3d22a87cb1e693a
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-02-23 00:36:55 +0330

    create FourInRow

--- app/src/main/java/com/example/game/FourInRowFragmentViewModel.kt
+++ app/src/main/java/com/example/game/FourInRowFragmentViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.game
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -12,6 +11,9 @@ class FourInRowFragmentViewModel : ViewModel() {
         arrayListOf("", "", "", "", ""),
         arrayListOf("", "", "", "", ""),
     )
+    private val listChoosePlayerOne = mutableSetOf<ArrayList<Int>>()
+    val listChoosePlayerTwo = mutableSetOf<ArrayList<Int>>()
+    var listCustom = arrayListOf(-1, -1)
     val listPlaceLiveData = MutableLiveData(listPlace)
 
     private var statusPlayer = StatusPlayerFourRow.PLAYER1
@@ -27,7 +29,9 @@ class FourInRowFragmentViewModel : ViewModel() {
             StatusPlayerFourRow.PLAYER1
         }
     }
-    fun chooseColumn(column : Int){
+
+    fun chooseColumn(column: Int) {
+        listCustom[0] = column
         listPlace[column] = chooseInColumn(listPlace[column])
         listPlaceLiveData.value = listPlace
     }
@@ -35,20 +39,108 @@ class FourInRowFragmentViewModel : ViewModel() {
     private fun chooseInColumn(list: ArrayList<String>): ArrayList<String> {
         for (i in list.indices) {
             if (list[i] == "") {
+                listCustom[1] = i
                 list[i] = choose()
                 return list
             }
         }
         return list
     }
-    private fun choose(): String{
-        return  if(statusPlayer == StatusPlayerFourRow.PLAYER1) {
+
+    private fun choose(): String {
+        return if (statusPlayer == StatusPlayerFourRow.PLAYER1) {
             changeStatus()
+            if (listCustom[1] != -1) {
+                listChoosePlayerOne.add(listCustom)
+            }
+            resetListCustom()
+            checkPlayerOneVertical()
+            checkPlayerOneHorizontal()
             namePlayerOne
-        }else{
+        } else {
             changeStatus()
+            if (listCustom[1] != -1) {
+                listChoosePlayerTwo.add(listCustom)
+            }
+            resetListCustom()
+            checkPlayerTowVertical()
+            checkPlayerTowHorizontal()
             namePlayerTwo
         }
     }
 
+    private fun checkPlayerOneVertical() {
+        var sum = 0
+        for (i in 0 until listPlace.size) {
+            for (j in 0 until listPlace[0].size) {
+                if (arrayListOf(i, j) in listChoosePlayerOne) {
+                    sum++
+                    if (sum == 4) {
+                        player.value = "$namePlayerOne Win"
+                    }
+                } else {
+                    sum = 0
+                }
+            }
+            sum = 0
+        }
+    }
+
+    private fun checkPlayerOneHorizontal() {
+        var sum = 0
+        for (i in 0 until listPlace.size) {
+            for (j in 0 until listPlace[0].size) {
+                if (arrayListOf(j, i) in listChoosePlayerOne) {
+                    sum++
+                    if (sum == 4) {
+                        player.value = "$namePlayerOne Win"
+                    }
+                } else {
+                    sum = 0
+                }
+            }
+            sum = 0
+        }
+    }
+
+    private fun checkPlayerTowVertical() {
+        var sum = 0
+        for (i in 0 until listPlace.size) {
+            for (j in 0 until listPlace[0].size) {
+                if (arrayListOf(i, j) in listChoosePlayerTwo) {
+                    sum++
+                    if (sum == 4) {
+                        player.value = "$namePlayerTwo Win"
+                    }
+                } else {
+                    sum = 0
+                }
+            }
+            sum = 0
+        }
+    }
+
+    private fun checkPlayerTowHorizontal() {
+        var sum = 0
+        for (i in 0 until listPlace.size) {
+            for (j in 0 until listPlace[0].size) {
+                if (arrayListOf(j, i) in listChoosePlayerTwo) {
+                    sum++
+                    if (sum == 4) {
+                        player.value = "$namePlayerTwo Win"
+                    }
+                } else {
+                    sum = 0
+                }
+            }
+            sum = 0
+        }
+    }
+
+
+
+    private fun resetListCustom() {
+        listCustom = arrayListOf(-1, -1)
+
+    }
 }
